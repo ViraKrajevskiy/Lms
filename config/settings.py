a9916c7047540d525d40ab44c760ecf00737e6ae
Vira <guidevirgate@gmail.com>
2025-04-26 19:43:18 +0500

    user

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'user_auth/static/'),  # Убедись, что эта директория существует
+    os.path.join(BASE_DIR, 'config/static/'),  # Убедись, что эта директория существует
 ]
 
 
