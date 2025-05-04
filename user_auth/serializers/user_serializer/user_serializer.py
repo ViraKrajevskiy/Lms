786eec76f115889fb1c0b78a20373495cf715df8
Vira <guidevirgate@gmail.com>
2025-05-04 10:41:59 +0500

    Последняя обнова

--- user_auth/serializers/user_serializer/user_serializer.py
+++ user_auth/serializers/user_serializer/user_serializer.py
@@ -2,14 +2,21 @@ from rest_framework import serializers
 from user_auth.models.base_user_model.user import User
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=False)
+    password = serializers.CharField(write_only=True, required=True)  # Обязательное поле для пароля
 
     class Meta:
         model = User
         fields = ['id', 'phone_number', 'email', 'role', 'is_active', 'password']
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True}  # Убедимся, что пароль не попадет в ответ
         }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')  # Получаем пароль
+        user = User(**validated_data)  # Создаем пользователя
+        user.set_password(password)  # Хешируем пароль
+        user.save()  # Сохраняем пользователя в базе
+        return user
 
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
